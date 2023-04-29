c746f11221562b3f74463f0383c9fb6e1c972051
hjsemirdzhyan <jsemirdzhyan@gmail.com>
2023-04-29 16:08:57 -0700

    Renaming and some experimenting of serialPrint timing
    Testing out if I can get serial print to be its own class to be called when needed. Not looking great so far.

---
+++ mainSketch/mainSketch.ino
@@ -0,0 +1,144 @@
+/*
+  This is where I'll copy/paste all my known working code. This is the eventual sketch getting uploaded to the full product. Hopefully.
+*/
+
+#include "dht.h"
+#define dht_apin A0
+dht DHT;
+const int pingPin = 7;  // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6;  // Echo Pin of Ultrasonic Sensor
+
+///////////////////////////////////Classes stored here///////////////////////////////////
+class Flasher {  // can probably reuse this for other LEDs that need to flash.
+  // Class Member Variables
+  // These are initialized at startup
+  int ledPin;    // the number of the LED pin
+  long OnTime;   // milliseconds of on-time
+  long OffTime;  // milliseconds of off-time
+
+  // These maintain the current state
+  int ledState;                  // ledState used to set the LED
+  unsigned long previousMillis;  // will store last time LED was updated
+
+  // Constructor - creates a Flasher
+  // and initializes the member variables and state
+public:
+  Flasher(int pin, long on, long off) {
+    ledPin = pin;
+    pinMode(ledPin, OUTPUT);
+
+    OnTime = on;
+    OffTime = off;
+
+    ledState = LOW;
+    previousMillis = 0;
+  }
+
+  void Update() {
+    // check to see if it's time to change the state of the LED
+    unsigned long currentMillis = millis();
+
+    if ((ledState == HIGH) && (currentMillis - previousMillis >= OnTime)) {
+      ledState = LOW;                  // Turn it off
+      previousMillis = currentMillis;  // Remember the time
+      digitalWrite(ledPin, ledState);        // Update the actual LED
+      Serial.print("LED State = ");
+      Serial.print(ledState);
+      Serial.print(" ");
+    } else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime)) {
+      ledState = HIGH;                 // turn it on
+      previousMillis = currentMillis;  // Remember the time
+      digitalWrite(ledPin, ledState);        // Update the actual LED
+      Serial.print("LED State = ");
+      Serial.print(ledState);
+      Serial.print(" ");
+    }
+  }
+};
+
+// ATTEMPTING to make this class a generic polling class that can be used for any sensor. Right now it works on the temp sensor. 
+class pollSensor {
+  long pollTime;               // how often in milliseconds to poll the temp sensor
+  unsigned long previousPoll;  // will store last time temp/humid reading was updated
+
+public:
+  pollSensor(long time) {
+    pollTime = time;
+    previousPoll = 0;
+  }
+
+  bool Update() {
+    unsigned long currentTime = millis();
+
+    if (currentTime - previousPoll >= pollTime) {
+      previousPoll = currentTime;
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+///////////////////////////////////Inits stored here///////////////////////////////////
+Flasher led1(13, 5000, 5000);
+pollSensor sensor1(5000);
+pollSensor sensor2PollDelay(100);
+pollSensor sensor2ClearTime(2);
+pollSensor sensor2BurstTime(10);
+
+///////////////////////////////////Setup and loop stored here///////////////////////////////////
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  led1.Update();
+  tempResults();
+  sonarResults();
+}
+
+///////////////////////////////////Functions stored here///////////////////////////////////
+void tempResults() {
+  if (sensor1.Update() == true) {
+    DHT.read11(dht_apin);
+    Serial.print("humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature);
+    Serial.println("C  ");
+  }
+}
+
+void sonarResults() {
+  if (sensor2PollDelay.Update() == true) {
+    long duration;
+    long inches;
+    long cm;
+    pinMode(pingPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    digitalWrite(pingPin, LOW);
+    if (sensor2ClearTime.Update() == true) {
+      digitalWrite(pingPin, HIGH);
+    }
+    if (sensor2BurstTime.Update() == true) {
+      digitalWrite(pingPin, LOW);
+    }
+    duration = pulseIn(echoPin, HIGH);
+    inches = duration / 74 / 2;
+    cm = duration / 29 / 2;
+    Serial.print(duration);
+    Serial.print("ms, ");
+    Serial.print(inches);
+    Serial.print("in, ");
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+  }
+}
+// void flashResults(int pin, int state) {
+//   digitalWrite(pin, state);
+//   Serial.print("LED State = ");
+//   Serial.print(state);
+//   Serial.print(" ");
+// }
---
+++ test_serialPrint_multitasking/test_serialPrint_multitasking.ino
@@ -0,0 +1,153 @@
+/*
+  The objective of this sketch is to make the serial print calls into a class or function that can be called given some interval input. Rather than it being called each time in every
+  function, I want to just call it once. It may need to be passed the displaying data too. It won't always be the same data type though so how do I make it modular enough to work
+  for any sensor? :thinking_face:
+*/
+
+#include "dht.h"
+#define dht_apin A0
+dht DHT;
+const int pingPin = 7;
+const int echoPin = 6;
+
+///////////////////////////////////Classes stored here///////////////////////////////////
+class Flasher {
+  int ledPin;
+  long OnTime;
+  long OffTime;
+  int ledState;
+  unsigned long previousMillis;
+public:
+  Flasher(int pin, long on, long off) {
+    ledPin = pin;
+    pinMode(ledPin, OUTPUT);
+
+    OnTime = on;
+    OffTime = off;
+
+    ledState = LOW;
+    previousMillis = 0;
+  }
+
+  void Update() {
+    unsigned long currentMillis = millis();
+    if ((ledState == HIGH) && (currentMillis - previousMillis >= OnTime)) {
+      ledState = LOW;
+      previousMillis = currentMillis;
+      digitalWrite(ledPin, ledState);
+      Serial.print("LED State = ");
+      Serial.print(ledState);
+      Serial.println(" ");
+    } else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime)) {
+      ledState = HIGH;
+      previousMillis = currentMillis;
+      digitalWrite(ledPin, ledState);
+      Serial.print("LED State = ");
+      Serial.print(ledState);
+      Serial.println(" ");
+    }
+  }
+};
+
+class pollSensor {
+  long pollTime;
+  unsigned long previousPoll;
+public:
+  pollSensor(long time) {
+    pollTime = time;
+    previousPoll = 0;
+  }
+  bool Update() {
+    unsigned long currentTime = millis();
+
+    if (currentTime - previousPoll >= pollTime) {
+      previousPoll = currentTime;
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+///////////////////////////////////Inits stored here///////////////////////////////////
+pollSensor led1OnTime(5000);
+pollSensor led1OffTIme(5000);
+pollSensor led1PollDelay(5000);
+pollSensor sensor1PollDelay(5000);
+pollSensor sensor2PollDelay(5000);
+pollSensor sensor2ClearTime(2);
+pollSensor sensor2BurstTime(10);
+
+///////////////////////////////////Setup and loop stored here///////////////////////////////////
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  ledResults();
+  tempResults();
+  sonarResults();
+}
+
+///////////////////////////////////Functions stored here///////////////////////////////////
+void tempResults() {
+  if (sensor1PollDelay.Update() == true) {
+    DHT.read11(dht_apin);
+    Serial.print("Humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("Temperature = ");
+    Serial.print(DHT.temperature);
+    Serial.println("C  ");
+  }
+}
+
+void sonarResults() {
+  if (sensor2PollDelay.Update() == true) {
+    long duration;
+    long inches;
+    long cm;
+    pinMode(pingPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    digitalWrite(pingPin, LOW);
+    if (sensor2ClearTime.Update() == true) {
+      digitalWrite(pingPin, HIGH);
+    }
+    if (sensor2BurstTime.Update() == true) {
+      digitalWrite(pingPin, LOW);
+    }
+    duration = pulseIn(echoPin, HIGH);
+    inches = duration / 74 / 2;
+    cm = duration / 29 / 2;
+    Serial.print("Range: ");
+    Serial.print(duration);
+    Serial.print("ms, ");
+    Serial.print(inches);
+    Serial.print("in, ");
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+  }
+}
+
+void ledResults() {
+  if (led1PollDelay.Update() == true) {
+    int ledPin;
+    long OnTime;
+    long OffTime;
+    int ledState;
+    if ((ledState == HIGH) && (led1OnTime.Update() == true)) {
+      ledState = LOW;
+      digitalWrite(ledPin, ledState);
+      Serial.print("LED State = ");
+      Serial.print(ledState);
+      Serial.println(" ");
+    } else if ((ledState == LOW) && (led1OffTIme.Update() == true)) {
+      ledState = HIGH;
+      digitalWrite(ledPin, ledState);
+      Serial.print("LED State = ");
+      Serial.print(ledState);
+      Serial.println(" ");
+    }
+  }
+}
