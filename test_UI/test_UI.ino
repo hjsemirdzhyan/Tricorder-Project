7a5f68e5ed00add7088257026e4d477f0e569fb6
hjsemirdzhyan <jsemirdzhyan@gmail.com>
2023-05-23 17:06:21 -0700

    Ongoing effort for UI stuff
    still working on it

--- test_UI/test_UI.ino
+++ test_UI/test_UI.ino
@@ -1,3 +1,10 @@
+/*
+  tft.setTextSize(2) size is 15 pixels
+  tft.setTextSize(1) size is 8 pixels
+
+
+*/
+
 #include <Adafruit_ST7735.h>
 #include <Arduino.h>
 
@@ -17,9 +24,9 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 int defaultOption = 1;
 int selectedOption = defaultOption;
-bool menuOpen = false;
 const uint16_t Display_Color = ST7735_BLUE;
 const uint16_t Text_Color = ST7735_WHITE;
+const uint16_t Sel_Color = ST7735_RED;
 
 
 // Classes ------------------------------------------------------------------------------------------------------------
@@ -52,6 +59,8 @@ class Menu {
   int position = 0;        // if it's a submenu, where in the list of its parent menu does this menu appear
   bool openState = false;  // true when open
   int numOfItems = 0;      // number of other menus or funcitons inside this one (is this even necessary anymore?)
+  bool menuOpen = false;
+
 public:
   Menu(int num, int belongs, int order, String name) {
     menuNumber = num;
@@ -68,7 +77,7 @@ public:
     for (int i = 0; i < arraySize; i++) {
       if (objects[i].menuNumber == menuNumber) {
         tft.println(objects[i].menuName);
-        for (int j = 1; j < arraySize; j++) {  // starting at 1 in order to ignore parent menu from being displayed as a list item inside itself. 
+        for (int j = 1; j < arraySize; j++) {  // starting at 1 in order to ignore parent menu from being displayed as a list item inside itself.
           tft.setTextSize(1);
           if (objects[j].belongsTo == menuNumber) {
             tft.println(objects[j].menuName);
@@ -77,9 +86,36 @@ public:
       }
     }
   }
+
+  void select(int menuNumber, int position)
 };
 
-// Initializations --------------------------------------------------------------
+class Navigate {
+  int selectedItem = 0;
+  int y_pos;
+  int newSelection;
+
+public:
+  Navigate() {
+  }
+
+  void goBack() {
+  }
+
+  void goForward() {  //aka 'select'
+  }
+
+  void goUp() {
+  }
+
+  void goDown() {
+    y_pos = (selectedItem * 10) + 22;
+    tft.drawRect(0, y_pos, 128, 10, Sel_Color);
+    selectedItem++;
+  }
+};
+
+// Initializations -------------------------------------------------------------------------------------------
 Menu objects[] = {
   Menu(0, 0, 0, "Main Menu"),
   Menu(1, 0, 1, "Enviro"),
@@ -92,11 +128,27 @@ Menu objects[] = {
 int numOfMenus = sizeof(objects) / sizeof(objects[0]);  // Some funky voodoo math going on here. ChatGPT gave this example to dynamically set the number of menus. It's working.
 
 DelayTimer bugginShiz(250);
+DelayTimer buttonDelay(500);
 
 // Functions -----------------------------------------------------------------------------------------------
-int buttonState() {
+void debugStuff() {
+  if (bugginShiz.Update() == true) {
+    Serial.print("selectedOption = ");
+    Serial.println(selectedOption);
+
+    //Serial.print("menuOpen = ");
+    //Serial.println(menuOpen);
+
+    Serial.print("cursorX&Y = ");
+    Serial.print(tft.getCursorX());
+    Serial.print(", ");
+    Serial.println(tft.getCursorY());
+  }
+}
+
+int buttonState() { //should this be a class?
   int state = analogRead(btn_apin);
-  if (buttonDelay.Update() == true) {
+  if (buttonDelay.Update() == true) {  // supposed to be a debounce...but not very effective.
     if (SW1 - RANGE <= state && state < RANGE + SW1) {
       return 1;
     }
@@ -117,86 +169,41 @@ int buttonState() {
   }
 }
 
-void startupMenu() {
-  tft.initR(INITR_144GREENTAB);
-  tft.fillScreen(Display_Color);
-  tft.setCursor(0, 0);
-  tft.setTextColor(Text_Color);
-  tft.setTextSize(2);
-  tft.println("Menu:");
-  tft.setTextSize(1);
-  tft.println("- Option 1");
-  tft.println("- Option 2");
-  tft.println("- Option 3");
-  tft.println("- Option 4");
-  //tft.drawRect(0, 15, 128, 10, ST7735_RED);
-}
-
-void option1Menu() {
-  tft.fillScreen(Display_Color);
-  tft.setCursor(0, 0);
-  tft.setTextColor(Text_Color);
-  tft.setTextSize(2);
-  tft.println("Option 1:");
-  tft.setTextSize(1);
-  tft.println("This is some stuff from option 1 menu");
-}
-
-void option2Menu() {
-  tft.fillScreen(Display_Color);
-  tft.setCursor(0, 0);
-  tft.setTextColor(Text_Color);
-  tft.setTextSize(2);
-  tft.println("Option 2:");
-  tft.setTextSize(1);
-  tft.println("How now brown cow?");
-}
-
-void openOption() {
-  if (selectedOption == 1) {
-    option1Menu();
-  }
-  if (selectedOption == 2) {
-    option2Menu();
-  }
-}
-
-void debugStuff() {
-  if (bugginShiz.Update() == true) {
-    Serial.print("selectedOption = ");
-    Serial.println(selectedOption);
-
-    Serial.print("menuOpen = ");
-    Serial.println(menuOpen);
-
-    Serial.print("cursorX&Y = ");
-    Serial.print(tft.getCursorX());
-    Serial.print(", ");
-    Serial.println(tft.getCursorY());
-  }
-}
-
-// Loops --------------------------------------------------------------------------
+// Loops ----------------------------------------------------------------------------------------------------
 void setup() {
   Serial.begin(9600);
   tft.initR(INITR_144GREENTAB);
   objects[0].draw(objects, numOfMenus);
-  delay(2000);
-  objects[1].draw(objects, numOfMenus);
-  delay(2000);
-  objects[2].draw(objects, numOfMenus);
+  //Menu of object[0], draw at position 1 which is at 15 pixels in the +y axis.
+
 }
 
 void loop() {
-
+  /*
+  if (buttonState() == 3) {
+    selectedOption++;
+  }
+  if (buttonState() == 2) {
+    selectedOption--;
+  }
+  if (buttonState() == 4) {
+    openOption();
+    menuOpen = true;
+  }
+  if (buttonState() == 1) {
+    if (menuOpen == true) {
+      startupMenu();
+      menuOpen = false;
+    } else {
+      selectedOption = defaultOption;
+    }
+  }
+  if (selectedOption > numOptions) {
+    selectedOption = defaultOption;
+  }
+  if (selectedOption < defaultOption) {
+    selectedOption = numOptions;
+  }
+  debugStuff();
+  */
 }
-
-
-
-
-/*  tft.println("- Option 1");
-    tft.println("- Option 2");
-    tft.println("- Option 3");
-    tft.println("- Option 4");
-    tft.drawRect(0, 15, 128, 10, ST7735_RED);
-    */
