e8388013a3e6b2a0c7d1c16d4e75b67a895c0cb9
hjsemirdzhyan <jsemirdzhyan@gmail.com>
2023-05-28 15:07:44 -0700

    Further improvements
    Still a ways to go.

--- test_UI_forEmulator/test_UI_forEmulator.ino
+++ test_UI_forEmulator/test_UI_forEmulator.ino
@@ -1,5 +1,6 @@
-/* Emulator can be found at:
-    https://wokwi.com/projects/308024602434470466
+/* 
+  Emulator can be found at: https://wokwi.com/projects/308024602434470466
+  C++ tutorial/examples: https://www.w3schools.com/cpp/cpp_oop.asp
 */
 
 #include "SPI.h"
@@ -61,8 +62,9 @@ class Menu {
   int position = 0;                                          // if it's a submenu, where in the list of its parent menu does this menu appear
   int numOfItems = 0;                                        // number of other menus or funcitons inside this one (is this even necessary anymore?)
   int currentSelectionPosition = selectionStartingPosition;  // keeps track of which menu item is selected
-  int endOfList = 0;                                         // Y position after the last item on a menu
+  int endOfList = 0;                                         // y_position after the last item on a menu
   int selectedItem = 1;                                      // numerical item currently selected
+  int numberOfListItems = 0;                                 // number of items on menu
 public:
   Menu(int menuNumber, int belongsTo, int position, String menuName) {
     this->menuNumber = menuNumber;
@@ -76,22 +78,28 @@ public:
     tft.setCursor(0, 0);
     tft.setTextColor(Text_Color);
     tft.setTextSize(2);
-    int count = 0;
     for (int i = 0; i < numOfMenus; i++) {
       if (objects[i].menuNumber == openMenu) {
         tft.println(objects[i].menuName);
+        int count = 1;
         for (int j = 1; j < numOfMenus; j++) {  // starting at 1 in order to ignore parent menu from being displayed as a list item inside itself. Need to replace numOfMenus with length of item list.
           tft.setTextSize(1);
           if (objects[j].belongsTo == menuNumber) {
             tft.println(objects[j].menuName);
+            count++;
           }
         }
+        numberOfListItems = count;
       }
     }
     endOfList = tft.getCursorY();
   }
 
-  int listSize(Menu* objects, int menuNum) {
+  int getNumberOfListItems(Menu* objects, int menuNum) {
+    return objects[getAddress(objects, menuNum)].numberOfListItems;
+  }
+
+  int listSize(Menu* objects, int menuNum) {  // redundant. Will delete and use the get instead.
     int sum = 0;
     for (int i = 1; i < numOfMenus; i++) {  // i = 1 instead of zero because we want to avoid counting the parent menu as a list item
       if (objects[i].belongsTo == menuNum) {
@@ -121,15 +129,23 @@ public:
     }
   }
 
-  int getSelectedItem(Menu* objects, int menuNum) {  // returns the menu number of the currently selected menu item
-    for (int i = 0; i < 10; i++) {
-      if (objects[i].menuNumber == menuNum) {
-        return objects[i].selectedItem;
+  int getSelectedMenu(Menu* objects) {                              //  returns the menu number of the currently selected menu item
+    for (int i = 1; i < numOfMenus; i++) {                          //  iterate over all menus
+      if (objects[i].belongsTo == getAddress(objects, openMenu)) {  //  first filter for belongsTo of current menu
+        if (objects[i].position == selectedItem) {                  //  Find where the position of the belongs to matches the position of the selected item.
+          return objects[i].menuNumber;                             //  return that that items menu number
+        }
       }
     }
   }
 
-  int getAddress(Menu* objects, int menuNum) { // this is only ever going to be used inside this method. Since you need to know the address already in order to call this method outside the class
+  int getSelectedItem(Menu* objects) {  //  returns the position in terms of the list lenght of the open menu
+    int a = getSelectedMenu(objects);   //  finds menu number of selectedMenu
+    int b = getAddress(objects, a);     //  find address of selectedMenu
+    return objects[b].position;         //  returns position number of selectedMenu
+  }
+
+  int getAddress(Menu* objects, int menuNum) {  // this is only ever going to be used inside this method. Since you need to know the address already in order to call this method outside the class
     for (int i = 0; i < numOfMenus; i++) {
       if (objects[i].menuNumber == menuNum) {
         return i;
@@ -146,19 +162,15 @@ public:
     currentSelectionPosition = currentSelectionPosition + selectionInterval;
     tft.drawRect(0, currentSelectionPosition, 128, 10, Sel_Color);
     selectedItem++;
+    Serial.print("selected item is ");
+    Serial.println(selectedItem);
   }
 
-  void goRight(Menu* objects, int selected) {  // AKA "Select"
-    for (int i = 0; i < numOfMenus; i++) {
-      if (objects[i].belongsTo == openMenu) {
-        for (int j = 0; j < numOfMenus; j++) {
-          if (objects[j].position == selected) {
-            openMenu = objects[j].menuNumber;
-            draw(objects);
-          }
-        }
-      }
-    }
+  void select(Menu* objects) { // sets the new open menu based on the selectedMenu
+    int a = getSelectedMenu(objects); //  gets the menu number of the selectedMenu
+    int b = getAddress(objects, a);
+    openMenu = objects[b].menuNumber;
+    draw(objects);
   }
 };
 
@@ -186,23 +198,25 @@ DelayTimer buttonDelay(500);
 // Functions -------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 void debugStuff() {
+  Serial.println("We're in the debugger");
   if (bugginShiz.Update() == true) {
-    int x = openMenu;
-    int y = menuToAddress(objects, openMenu);
-    String a = objects[y].getMenuName(objects, openMenu);
-    int b = objects[y].getMenuBelongsTo(objects, openMenu);
-    String c = objects[b].getMenuName(objects, b);
-    int d = objects[y].listSize(objects, openMenu);
-    int e = objects[y].getSelectedItem(objects, openMenu);
 
-    objects[openMenu].cursorToEndOfList();
+    int x = openMenu;                                        // shows currently active menu
+    int y = menuToAddress(objects, openMenu);                // converts menu number to its address
+    String a = objects[y].getMenuName(objects, openMenu);    // gets menu name
+    int b = objects[y].getMenuBelongsTo(objects, openMenu);  // gets menu number of parent menu
+    String c = objects[b].getMenuName(objects, b);           // gets name of parent menu
+    int d = objects[y].getNumberOfListItems(objects, y);     // gets count of listed items
+    int e = objects[y].getSelectedItem(objects);             // gets position of selected item
+
+    objects[y].cursorToEndOfList();
 
     tft.print("This is menu number ");
     tft.print(x);
     tft.print(" which is named ");
     tft.print(a);
     tft.print(". It belongs to menu number ");
-    tft.print(b);  // >_<
+    tft.print(b);
     tft.print(", also known as ");
     tft.print(c);
     tft.print(". There are ");
@@ -238,7 +252,7 @@ int buttonState() {  //should this be a class?
   }
 }
 
-int menuToAddress(Menu* objects, int menuNum) { // this function is needed in order to get the unknown address of a known menu number. The class memeber getAddress requires you already to know the address. 
+int menuToAddress(Menu* objects, int menuNum) {  // this function is needed in order to get the unknown address of a known menu number. The class memeber getAddress requires you already to know the address.
   for (int i = 0; i < numOfMenus; i++) {
     if (objects[i].getMenuNumber(objects, i) == menuNum) {
       return i;
@@ -247,16 +261,20 @@ int menuToAddress(Menu* objects, int menuNum) { // this function is needed in or
 }
 
 void testingActions() {  // temporary until I get my buttons to work.
-  int length = objects[openMenu].listSize(objects, openMenu);
+  int a = menuToAddress(objects, openMenu); //  address of open menu
+  int length = objects[a].getNumberOfListItems(objects, openMenu);
   for (int i = 0; i < length - 1; i++) {  // simulates moving the cursor down the length of the current menus list
-    objects[openMenu].goDown(objects);
-    delay(1000);
+    objects[a].goDown(objects);
+    delay(250);
   }
+  Serial.println("We're done moving");
+  delay(1000);
+  objects[a].select(objects); //
 }
 
 void startUp() {
   tft.begin();
-  objects[startingMenu].draw(objects);                 // Edit "startingMenu" to change which menu appears on screen
+  objects[startingMenu].draw(objects);                             // Edit "startingMenu" to change which menu appears on screen
   tft.drawRect(0, selectionStartingPosition, 128, 10, Sel_Color);  // Draws selection rectangle on startingPosition of the screen
 }
 
@@ -269,7 +287,6 @@ void setup() {
   startUp();
   testingActions();  // For now, will be simulating button presses via hard coded functions
   debugStuff();
-  Serial.println(numOfMenus);
 }
 
 void loop() {
